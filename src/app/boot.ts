760fb280f9c37ef77d493100c376892e8101b736
renato <neo_rlm@hotmail.com>
2024-11-22 22:17:02 -0300

    update

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -1,7 +1,7 @@
 import { Application } from "express";
 import express from "./express";
 import modules from "./modules";
-import config from '../confg/config';
+import config from '../config/config';
 export default async function bootstrap(app: Application): Promise<void> {
   await express(app, config);
   await modules(app)
--- src/libs/wbot.ts
+++ src/libs/wbot.ts
@@ -1,5 +1,5 @@
 import { Whatsapp , create} from "@wppconnect-team/wppconnect";
-import config from "../confg/config";
+import config from "../config/config";
 
 interface Session extends Whatsapp {
     id: number;
@@ -11,18 +11,29 @@ interface Session extends Whatsapp {
   export const initWbot = async (whatsapp: Whatsapp): Promise<Session> => {
     return  new Promise((resolve, reject) => {
         try {
-            
+           
             const wbot = create(
                 Object.assign({},
                     config.createOptions,
                     {
-                        sessions: "renato"
+                        session: "renato"
                     }
                 )
             ) as unknown as Session
+            sessions.push(wbot)
+            console.log(sessions)
             resolve(wbot)
         } catch (error) {
             
         }
      })
    }
+
+   export const getWbot = (whatsappId: number): Session => {
+    const sessionIndex = sessions.findIndex((s) => s.id === Number(whatsappId));
+    if (sessionIndex === -1) {
+      throw new Error("ERR_WAPP_NOT_INITIALIZED");
+    }
+  
+    return sessions[sessionIndex];
+  };
